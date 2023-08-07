6a34a9f2232eeccb992bc8f393a128bf7932de97
Tunç Yılmaz Türkoğlu <tunc.yt.93@gmail.com>
2023-08-07 21:30:19 +0300

    Harpoon update

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,20 @@
+local M = {
+  'ThePrimeagen/harpoon',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    "nvim-telescope/telescope.nvim",
+  },
+}
+
+function M.setup()
+  local opts = { noremap = true, silent = true }
+  local keymap = vim.keymap.set
+  local telescope = require('telescope').load_extension('harpoon')
+  keymap("n", "<leader>ha", ":lua require('harpoon_mark').add_file()<CR>", opts)
+  keymap("n", "<leader>hu", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+  keymap("n", "<leader>hn", ":lua require('harpoon.ui').nav_next()<CR>", opts)
+  keymap("n", "<leader>hp", ":lua require('harpoon.ui').nav_prev()<CR>", opts)
+  keymap("n", "<leader>nf", ":lua require('harpoon.ui').nav_file()<CR>", opts)
+end
+
+return M
