979099e0b4b7fa1c4d3ee4e1b6baff01833051ed
hereticrush <auroraborealis853@gmail.com>
2023-09-29 18:12:15 +0300

    migration: migrate to indent_blankline version 3

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,10 +3,10 @@ local M = {
   event = "VeryLazy",
   opts = {
     timeout = 2000,
-    fps = 60,
+    fps = 30,
 
     max_height = function()
-      return math.floor(vim.o.lines * 0.75)
+      return math.floor(vim.o.lines * 0.60)
     end,
     max_width = function()
       return math.floor(vim.o.columns * 0.75)
