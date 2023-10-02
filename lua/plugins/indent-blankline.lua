f217bd96638f4a244cc62667b0202740105fb1dd
hereticrush <auroraborealis853@gmail.com>
2023-10-02 15:09:24 +0300

    update: options ibl version 3

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,6 +2,7 @@ local M = {
   "lukas-reineke/indent-blankline.nvim",
   event = "BufReadPre",
   main = "ibl",
+<<<<<<< HEAD
   opts = {},
   indent = { highlight = { "CursorColumn", "Whitespace" }, char = "" },
   whitespace = {
@@ -35,6 +36,32 @@ local M = {
 			"coc-explorer",
 		},
 	}
+=======
+  opts = {
+    indent = {
+      highlight = { "CursorColumn", "Whitespace" },
+      char = "|",
+      tab_char = ".",
+    },
+    whitespace = {
+      highlight = { "Function", "Label" },
+      remove_blankline_trail = true,
+    },
+    scope = {
+      enabled = true,
+      include = {
+        node_type = {
+          c = { "return_statement" },
+          lua = { "return_statement", "table_constructor" },
+        },
+      },
+    },
+    debounce = 100,
+    exclude = {
+      buftypes = { 'terminal', 'nofile', 'quickfix', 'prompt' },
+    },
+  },
+>>>>>>> c70e53e (update: options ibl version 3)
 }
 
 return M
