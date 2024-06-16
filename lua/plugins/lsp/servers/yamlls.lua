f7e28debf182a0f38d8973733f0d6dc3d2c35336
hereticrush <auroraborealis853@gmail.com>
2024-06-16 18:50:01 +0300

    Update lsp

--- lua/plugins/lsp/servers/yamlls.lua
+++ lua/plugins/lsp/servers/yamlls.lua
@@ -1,1 +1,8 @@
-return function() end
+return function(on_attach)
+  return {
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      client.server_capabilities.document_formatting = true
+    end,
+  }
+end
